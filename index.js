602e2b72e75dcabf0376f26012151f3354a693aa
Afrisal Yodi Purnama <afrisalyp@gmail.com>
2017-08-08 14:23:35 +0700

    Define default object.

---
+++ index.js
@@ -0,0 +1,9 @@
+import {
+  Image
+} from 'react-native';
+
+const CircleImage = (props) => {
+  <Image source={{uri: 'https://s.kompetivo.com/88384bd3.jpg'}} style={{width: 22, height: 22}} />
+};
+
+export default CircleImage;
