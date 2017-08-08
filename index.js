6625f8d7d4f6be47d588c25bc24a2c1bfc9d86b1
Afrisal Yodi Purnama <afrisalyp@gmail.com>
2017-08-08 18:02:25 +0700

    Return jsx.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {
 } from 'react-native';
 
 const CircleImage = (props) => {
-  <Image source={{uri: 'https://s.kompetivo.com/88384bd3.jpg'}} style={{width: 22, height: 22}} />
+  return (<Image source={{uri: 'https://s.kompetivo.com/88384bd3.jpg'}} style={{width: 22, height: 22}} />);
 };
 
 export default CircleImage;
